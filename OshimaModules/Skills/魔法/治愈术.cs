c3fbad2b7b156afb220360a6587786350667b375
milimoe <mili@wrss.org>
2024-11-07 00:57:44 +0800

    bug

--- OshimaModules/Skills/魔法/治愈术.cs
+++ OshimaModules/Skills/魔法/治愈术.cs
@@ -21,7 +21,7 @@ namespace Oshima.FunGame.OshimaModules.Skills
         public 治愈术(Character? character = null) : base(SkillType.Magic, character)
         {
             SelectTargetPredicates.Add(c => c.HP > 0 && c.HP < c.MaxHP);
-            Effects.Add(new 百分比回复生命值(this, 0.3, 0.3));
+            Effects.Add(new 百分比回复生命值(this, 0.03, 0.03));
         }
     }
 }
