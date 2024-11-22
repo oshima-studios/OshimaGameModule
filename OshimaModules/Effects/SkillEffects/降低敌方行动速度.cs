069f85eff552a6840c8cdc6c47a6fe6592645bea
milimoe <mili@wrss.org>
2024-11-22 23:58:11 +0800

    修正符号

--- OshimaModules/Effects/SkillEffects/降低敌方行动速度.cs
+++ OshimaModules/Effects/SkillEffects/降低敌方行动速度.cs
@@ -7,7 +7,7 @@ namespace Oshima.FunGame.OshimaModules.Effects.SkillEffects
     {
         public override long Id => Skill.Id;
         public override string Name => Skill.Name;
-        public override string Description => $"降低目标{(Skill.CanSelectTargetCount > 1 ? $"至多 {Skill.CanSelectTargetCount} 个" : "")}敌人 {SPD:0.##} 点行动速度。";
+        public override string Description => $"降低目标{(Skill.CanSelectTargetCount > 1 ? $"至多 {Skill.CanSelectTargetCount} 个" : "")}敌人 {Math.Abs(SPD):0.##} 点行动速度。";
 
         private double SPD { get; set; } = 0;
         private string 持续时间 => _durative && _duration > 0 ? _duration + " 时间" : (!_durative && _durationTurn > 0 ? _durationTurn + " 回合" : "0 时间");
