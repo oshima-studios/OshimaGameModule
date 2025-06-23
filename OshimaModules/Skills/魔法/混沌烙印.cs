23d14fcdb8581d060032030e516a2fccd86a8aec
milimoe <mili@wrss.org>
2025-06-23 23:39:47 +0800

    削弱气绝

--- OshimaModules/Skills/魔法/混沌烙印.cs
+++ OshimaModules/Skills/魔法/混沌烙印.cs
@@ -12,13 +12,13 @@ namespace Oshima.FunGame.OshimaModules.Skills
         public override string DispelDescription => Effects.Count > 0 ? Effects.First(e => e is 造成气绝).DispelDescription : "";
         public override double MPCost => Level > 0 ? 65 + (70 * (Level - 1)) : 65;
         public override double CD => Level > 0 ? 65 - (1.5 * (Level - 1)) : 65;
-        public override double CastTime => 9;
-        public override double HardnessTime { get; set; } = 7;
+        public override double CastTime => 4;
+        public override double HardnessTime { get; set; } = 5;
         public override int CanSelectTargetCount => 1;
 
         public 混沌烙印(Character? character = null) : base(SkillType.Magic, character)
         {
-            Effects.Add(new 造成气绝(this, true, 4, 0, 0.5, true, 0, 0.3));
+            Effects.Add(new 造成气绝(this, true, 4, 0, 0.5, true, 0, 0.05));
         }
     }
 }
